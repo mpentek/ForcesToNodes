c65621f9c3c144aa64cf76009ff97789eb4d490b
Mate Pentek <mpentek@gmail.com>
2023-02-15 09:49:00 +0100

    Adding global defitions 2d

--- fem_analogy_2d_utilities.py
+++ fem_analogy_2d_utilities.py
@@ -4,6 +4,10 @@ import numpy as np
 #############################
 # OWN function definition START
 
+DIMENSION = 2
+DOFS_PER_NODE = 3
+ERR_ABS_TOL = 1e-10
+
 def setup_fem_beam_analogy(nodal_coordinates, nodes_geom_center, E=10000.0, A=100.0, I=1000.0):
     
     def get_length_and_angle_coefs(p1, p2):
@@ -14,7 +18,7 @@ def setup_fem_beam_analogy(nodal_coordinates, nodes_geom_center, E=10000.0, A=10
         
         return length, cos_val, sin_val
     
-    k_total_global =np.zeros((3 + 2*len(nodal_coordinates), 3 + 2*len(nodal_coordinates)))
+    k_total_global =np.zeros((DOFS_PER_NODE + DIMENSION*len(nodal_coordinates), DOFS_PER_NODE + DIMENSION*len(nodal_coordinates)))
     
     for idx, node in enumerate(nodal_coordinates):
         L, c_val, s_val = get_length_and_angle_coefs(nodes_geom_center, node)
@@ -25,40 +29,41 @@ def setup_fem_beam_analogy(nodal_coordinates, nodes_geom_center, E=10000.0, A=10
         k_rr = 3*E*I/L
         
         k_elem_local = np.array([
+            # first node - displacement and rotation
             [ k_u,   0.0,   0.0, -k_u,    0.0],
             [ 0.0,  k_vv,   k_vr,  0.0,  -k_vv],
             [ 0.0,  k_vr,   k_rr,  0.0,  -k_vr],
+            # second node - only displacements
             [-k_u,   0.0,   0.0,  k_u,    0.0],
             [ 0.0, -k_vv,  -k_vr,  0.0,   k_vv]])
         
         t_elem = np.array([
-            [ c_val, s_val, 0.0,   0.0,    0.0],
-            [ -s_val,  c_val, 0.0,   0.0,    0.0],
-            [   0.0,   0.0,  1.0,   0.0,    0.0],
-            [   0.0,   0.0,  0.0,  c_val, s_val],
-            [   0.0,   0.0,  0.0,  -s_val,  c_val]])
+            [  c_val, s_val, 0.0,   0.0,    0.0],
+            [ -s_val, c_val, 0.0,   0.0,    0.0],
+            [    0.0,   0.0, 1.0,   0.0,    0.0],
+            [    0.0,   0.0, 0.0,  c_val, s_val],
+            [    0.0,   0.0, 0.0, -s_val, c_val]])
         
         k_elem_global = np.matmul(np.matmul(np.transpose(t_elem), k_elem_local), t_elem)
         
         # add diagonally-clustered entries corresponding to the starting node - i.e center node - of the beam
-        for i in range(3):
-            for j in range(3):
+        # forces and moments
+        for i in range(DOFS_PER_NODE):
+            for j in range(DOFS_PER_NODE):
                 k_total_global[i,j] += k_elem_global[i,j]
                 
-                # print(np.array_str(k_total_global[0:10,0:10], precision=2, suppress_small=True))
-        
         # add diagonally-clustered entries corresponding to the end node of of the beam
-        for i in range(2):
-            for j in range(2):
-                k_total_global[3 + idx*2 + i, 3 + idx*2 + j] += k_elem_global[3+i,3+j]
-    
+        for i in range(DIMENSION):
+            for j in range(DIMENSION):
+                k_total_global[DOFS_PER_NODE + idx*DIMENSION + i, DOFS_PER_NODE + idx*DIMENSION + j] += k_elem_global[DOFS_PER_NODE+i,DOFS_PER_NODE+j]
+                
         # add coupling terms between nodes, which are off-diagonal
-        for i in range(2):
-            for j in range(3):
+        for i in range(DIMENSION):
+            for j in range(DOFS_PER_NODE):
                 # lower diagonal
-                k_total_global[3 + idx*2 + i, j] += k_elem_global[3+i,j]
+                k_total_global[DOFS_PER_NODE + idx*DIMENSION + i, j] += k_elem_global[DOFS_PER_NODE+i,j]
                 # upper diagonal
-                k_total_global[j, 3 + idx*2 + i] += k_elem_global[j,3+i]
+                k_total_global[j, DOFS_PER_NODE + idx*DIMENSION + i] += k_elem_global[j,DOFS_PER_NODE+i]
     
     return k_total_global
 
@@ -74,28 +79,28 @@ def map_forces_to_nodes(stiffness_matrix, target_resultants):
     
     ########
     # solve
-    center_node_deformations = np.linalg.solve(stiffness_matrix[:3,:3], target_resultants)
+    center_node_deformations = np.linalg.solve(stiffness_matrix[:DOFS_PER_NODE,:DOFS_PER_NODE], target_resultants)
     
     # setup the deformation vector - apart from the center node deformations
     # all are zero translations due to the pinned support
     all_deformations = np.zeros(stiffness_matrix.shape[0])
     # only nonzero which were previously solved for
-    all_deformations[:3] = center_node_deformations
+    all_deformations[:DOFS_PER_NODE] = center_node_deformations
     
     # recover all forces
     all_forces = np.dot(stiffness_matrix,all_deformations)
     # first 3 are the recovered resultants
-    recovered_resultants = all_forces[:3]
+    recovered_resultants = all_forces[:DOFS_PER_NODE]
     # the remaining are the actual unknowns of the problem
     # return the reaction forces from each pinned node
     # flip sign for consistency
-    nodal_forces = -all_forces[3:]
+    nodal_forces = -all_forces[DOFS_PER_NODE:]
     
     # check target forces in the center node
     residual = np.subtract(target_resultants, recovered_resultants)
     norm_of_residual = np.linalg.norm(residual)
-    print(norm_of_residual)
-    if norm_of_residual > 1e-4:
+    print("Residual check in map_forces_to_nodes: " , str(norm_of_residual))
+    if norm_of_residual > ERR_ABS_TOL:
         raise Exception("Norm of residual too large, check algorithm!")
 
     return nodal_forces, center_node_deformations
@@ -106,12 +111,12 @@ def check_resultant(nodal_coordinates, nodes_geom_center, nodal_forces, target_r
     # check based on nodal position
     n_nodes = len(nodal_coordinates)
 
-    mapping_coef_matrix = np.zeros((3, 2*n_nodes))
+    mapping_coef_matrix = np.zeros((DOFS_PER_NODE, DIMENSION*n_nodes))
 
     # fx contribution
-    mapping_coef_matrix[0, 0::2] = 1.0
+    mapping_coef_matrix[0, 0::DIMENSION] = 1.0
     # fy contribution
-    mapping_coef_matrix[1, 1::2] = 1.0
+    mapping_coef_matrix[1, 1::DIMENSION] = 1.0
     # mz
     for i in range(0, n_nodes):
         # dx, dy
@@ -120,18 +125,18 @@ def check_resultant(nodal_coordinates, nodes_geom_center, nodal_forces, target_r
 
         # mz = dx * fy - dy * fx
         # fx contribution
-        mapping_coef_matrix[2, 2*i+0] = -dy
+        mapping_coef_matrix[2, DIMENSION*i+0] = -dy
         # fy contribution
-        mapping_coef_matrix[2, 2*i+1] = dx
+        mapping_coef_matrix[2, DIMENSION*i+1] = dx
 
     recovered_resultants = np.dot(mapping_coef_matrix, nodal_forces)
     residual = np.subtract(recovered_resultants, target_resultants)
     norm_of_residual = np.linalg.norm(residual)
     print("Residual check in check_resultants: " , str(norm_of_residual))
-    if norm_of_residual > 1e-4:
+    if norm_of_residual > ERR_ABS_TOL:
         raise Exception("Norm of residual too large, check algorithm!")
 
-    return not(norm_of_residual > 1e-4)
+    return not(norm_of_residual > ERR_ABS_TOL)
 
 if __name__ == "__main__":
     # generating nodal coordinates and target resultants
