7773d93e7a1673d203aab6bc3804e163a86ce9e1
mpentek <mpentek@gmail.com>
2023-03-20 13:50:11 +0100

    Working OpenMP

--- test3dcsm.gid/apply_level_force_process.py
+++ test3dcsm.gid/apply_level_force_process.py
@@ -1,21 +1,9 @@
-#===============================================================================
-'''
-Project:Lecture - Structural Wind Engineering WS19-20
-        Chair of Structural Analysis @ TUM - R. Wuchner, M. Pentek
-
-Author: mate.pentek@tum.de
-
-Description: Kratos level forces in body- and flow-attached coordinates
-
-Created on:  06.01.2020
-Last update: 06.01.2020
-'''
-#===============================================================================
-
 import KratosMultiphysics
 import KratosMultiphysics.StructuralMechanicsApplication as KSM
 from KratosMultiphysics.time_based_ascii_file_writer_utility import TimeBasedAsciiFileWriterUtility
-import math
+
+from fem_analogy_3d_utilities import setup_fem_beam_analogy, map_forces_to_nodes, check_resultant 
+
 import numpy as np
 import os
 
@@ -24,7 +12,6 @@ def Factory(params, Model):
         raise Exception(
             'expected input shall be a Parameters object, encapsulating a json string')
     return ApplyLevelForceProcess(Model, params["Parameters"])
-
 
 class ApplyLevelForceProcess(KratosMultiphysics.Process):
     '''
@@ -43,9 +30,12 @@ class ApplyLevelForceProcess(KratosMultiphysics.Process):
                 "input_folder" : "",
                 "input_file_prefix" : "",
                 "input_file_extension": ".dat",
+                "rampup_time_upper_limit" : 1e30,
                 "nr_input_intervals": 1,
                 "column_ids" : [],
                 "swap_sign" : false,
+                "print_format"          : ".8f",
+                "print_to_screen"       : true,
                 "write_output_file"     : true,
                 "output_file_settings"  : {}
             }
@@ -53,14 +43,20 @@ class ApplyLevelForceProcess(KratosMultiphysics.Process):
 
         params.ValidateAndAssignDefaults(default_settings)
 
-        # modal part params
+        #############
+        # on all ranks
+        # could be carried out only on rank 0
+
         self.model_part_name = params['model_part_name'].GetString()
         self.model_part = Model[self.model_part_name]
         
         input_folder = params['input_folder'].GetString()
         input_file_prefix = params['input_file_prefix'].GetString()
         input_file_extension = params['input_file_extension'].GetString()
-        nr_input_intervals = params['nr_input_intervals'].GetInt()
+        self.nr_input_intervals = params['nr_input_intervals'].GetInt()
+        
+        self.time_limit = params['rampup_time_upper_limit'].GetDouble()
+        
         column_ids = params['column_ids'].GetVector()
         swap_sign = params['swap_sign'].GetBool()
         if swap_sign:
@@ -68,27 +64,63 @@ class ApplyLevelForceProcess(KratosMultiphysics.Process):
         else:
             sign_multiplier = 1
 
-        force_labels = ['fx','fy','fz','mx','my','mz']
+        self.force_labels = ['fx','fy','fz','mx','my','mz']
 
+        self.print_to_screen = params['print_to_screen'].GetBool()
+        self.write_output_file = params['write_output_file'].GetBool()
+        self.format = params["print_format"].GetString()
+
+        #############
+        # on rank 0
         if (self.model_part.GetCommunicator().MyPID() == 0):
-            level_forces = {}
+            
+            #############
+            # initialize level force output
+            if (self.write_output_file):
+
+                # create/check/assign file name prefix
+                output_file_name_prefix = params["model_part_name"].GetString() + "_level_force_"
+
+                file_handler_params = KratosMultiphysics.Parameters(
+                    params["output_file_settings"])
+
+                if file_handler_params.Has("file_name"):
+                    warn_msg = 'Unexpected user-specified entry found in "output_file_settings": {"file_name": '
+                    warn_msg += '"' + \
+                        file_handler_params["file_name"].GetString(
+                        ) + '"}\n'
+                    warn_msg += 'Using this specififed file name instead of the default "' + \
+                        output_file_name_prefix + '"'
+                    KratosMultiphysics.Logger.PrintWarning(
+                        "ComputeLevelForceProcess", warn_msg)
+
+                    output_file_name_prefix = file_handler_params["file_name"].GetString() + "_level_force_"
+                else:
+                    file_handler_params.AddEmptyValue("file_name")
+                
+            #############
+            # initialize level force input
+            self.level_forces = {}
+            
+            # NOTE: just for a check
             accum_level_nodes = 0
             
-            for l_id in range(nr_input_intervals):
-                level_forces[l_id] = {}
+            for l_id in range(self.nr_input_intervals):
+                self.level_forces[l_id] = {}
                 file_name = os.path.join(input_folder, input_file_prefix + str(l_id) + input_file_extension)
                 
                 # read in time series    
-                level_forces[l_id]['time'] = np.loadtxt(file_name, usecols =(0,))
+                # NOTE: we might not need time
+                self.level_forces[l_id]['time'] = np.loadtxt(file_name, usecols =(0,))
                 # read force and moment input
                 for c_id in range(column_ids.Size()):
-                    level_forces[l_id][force_labels[c_id]] = np.multiply(np.loadtxt(file_name,
+                    self.level_forces[l_id][self.force_labels[c_id]] = np.multiply(np.loadtxt(file_name,
                                                                         usecols =(int(column_ids[c_id])),), sign_multiplier)
 
                 # adding descriptors
                 line_nr = 0
                 descriptive_lines = []
-                with open(file_name, 'r+') as fp:
+                with open(file_name, 'r') as fp:
                     while line_nr < 4:
                         line = fp.readline()
                         descriptive_lines.append(line.replace(',','').replace('\n','').split(' '))
@@ -96,74 +128,144 @@ class ApplyLevelForceProcess(KratosMultiphysics.Process):
 
                 if not l_id == int(descriptive_lines[0][-1]):
                     raise Exception('Mismatch between intended and read level id!')
-                level_forces[l_id]['start_id'] = [float(val) for val in descriptive_lines[1][2:]]
-                level_forces[l_id]['center_id'] = [float(val) for val in descriptive_lines[2][2:]]
-                level_forces[l_id]['end_id'] = [float(val) for val in descriptive_lines[3][2:]]
+                self.level_forces[l_id]['start_coords'] = [float(val) for val in descriptive_lines[1][2:]]
+                self.level_forces[l_id]['center_coords'] = [float(val) for val in descriptive_lines[2][2:]]
+                self.level_forces[l_id]['end_coords'] = [float(val) for val in descriptive_lines[3][2:]]
+                
+                ###
+                # NOTE: the following block does not take care properly of MPI
+                # nodes (with IDs and coordinates) are local to ranks
+                # the stiffnes matrix needs to be assembled globally
+                ###
                 
                 # adding node IDs from model part
-                level_forces[l_id]['node_ids'] = []
-                level_forces[l_id]['node_coords'] = []
+                # by filtering Z coordinates - here along the height
+                self.level_forces[l_id]['node_ids'] = []
+                self.level_forces[l_id]['node_coords'] = []
                 for node in self.model_part.Nodes:
-                    if level_forces[l_id]['start_id'][-1] <= node.Z0 and node.Z0 < level_forces[l_id]['end_id'][-1]:
-                        level_forces[l_id]['node_ids'].append(node.Id)
-                        level_forces[l_id]['node_coords'].append([node.X0, node.Y0, node.Z0])
-                accum_level_nodes += len(level_forces[l_id]['node_ids'])       
-                print()
+                    if self.level_forces[l_id]['start_coords'][-1] <= node.Z0 and node.Z0 < self.level_forces[l_id]['end_coords'][-1]:
+                        self.level_forces[l_id]['node_ids'].append(node.Id)
+                        self.level_forces[l_id]['node_coords'].append([node.X0, node.Y0, node.Z0])
+                accum_level_nodes += len(self.level_forces[l_id]['node_ids'])       
+                #############
                 
-            myval = len(self.model_part.Nodes)
+                #############
+                # initialize mapping matrix using the FEM analogy
+                # NOTE: the geometric center might be different
+                self.level_forces[l_id]['stiffness_matrix'] = setup_fem_beam_analogy(self.level_forces[l_id]['node_coords'], 
+                                                                                self.level_forces[l_id]['center_coords'])
+            
+
+                #############
+                # initialize level force output
+                if (self.write_output_file):
+                    # file for each level
+                    file_handler_params["file_name"].SetString(
+                            output_file_name_prefix + str(l_id) + '.dat')
+                    file_header = self._GetFileHeader(l_id)
+
+                    self.level_forces[l_id]['output_file'] = TimeBasedAsciiFileWriterUtility(self.model_part,
+                                                                                        file_handler_params, file_header).file
+
+            
+            # NOTE: for an internal check  
             if not accum_level_nodes == len(self.model_part.Nodes):
                 raise Exception('Mismatch between accumulated and total number of nodes!')
-            print()
+
+    def ExecuteInitializeSolutionStep(self):
+        
+        # NOTE: check if whe should base it on time
+        # linear time ramp-up
+        current_time = self.model_part.ProcessInfo[KratosMultiphysics.TIME]
+        time_fctr = 1.0
+        if current_time <= self.time_limit:
+            time_factor = current_time/self.time_limit
+            
+        step = self.model_part.ProcessInfo[KratosMultiphysics.STEP]
+        
+        #############
+        # on rank 0
+        if (self.model_part.GetCommunicator().MyPID() == 0):
+            for l_id in range(self.nr_input_intervals):
+            
+                #############
+                # initialize level force input
+                # NOTE: we might want to use a ramp-up function of the force input
+                target_resultants = np.zeros(len(self.force_labels))
+                for c, f_l in enumerate(self.force_labels):
+                    target_resultants[c] = time_fctr * self.level_forces[l_id][f_l][step]
+                
+                # nodal forces need to be calculates for each given set of target resultant    
+                nodal_forces, _ = map_forces_to_nodes(self.level_forces[l_id]['stiffness_matrix'], target_resultants)
+                perform_check = check_resultant(self.level_forces[l_id]['node_coords'], self.level_forces[l_id]['center_coords'], nodal_forces, target_resultants)
+                print("Perform final check: ", str(perform_check))
+            
+                #############
+                # apply as in Kratos
+                
+                ###
+                # NOTE: the following block does not take care properly of MPI
+                # nodal forces can be calculated glocally on rank 0
+                # the result and distribution needs to happen for each node on respective ranks
+                # so local node IDs
+                ###
+                
+                nodal_point_load_val = KratosMultiphysics.Vector(3)            
+                for c, node_id in enumerate(self.level_forces[l_id]['node_ids']):
+                    for i in range(nodal_point_load_val.Size()):
+                        nodal_point_load_val[i] = nodal_forces[c*3 + i]
+                    # for StructuralMechanics: each node has a corresponding 1D condition, which has POINT_LOAD as an assigned value
+                    self.model_part.Conditions[node_id].SetValue(KSM.POINT_LOAD, nodal_point_load_val)
             
     def ExecuteFinalizeSolutionStep(self):
 
         current_time = self.model_part.ProcessInfo[KratosMultiphysics.TIME]
 
-        if((current_time >= self.interval[0]) and (current_time < self.interval[1])):
-            for idx in range(len(self.levels)):
-                fb, mb = self._EvaluateLevelForces(idx)
+        for l_id in range(self.nr_input_intervals):
+            fb, mb = self._EvaluateLevelForces(l_id)
 
-                if (self.model_part.GetCommunicator().MyPID() == 0):
-                    output = []
-                    output.extend(fb)
-                    output.extend(mb)
+            if (self.model_part.GetCommunicator().MyPID() == 0):
+                output = []
+                output.extend(fb)
+                output.extend(mb)
 
-                    output_vals = [format(val, self.format) for val in output]
-                    # not formatting time in order to not lead to problems with time recognition
-                    # in the file writer when restarting
-                    output_vals.insert(0, str(current_time))
+                output_vals = [format(val, self.format) for val in output]
+                # not formatting time in order to not lead to problems with time recognition
+                # in the file writer when restarting
+                output_vals.insert(0, str(current_time))
 
-                    res_labels = ['time: ',
-                                  'fx\': ', 'fy\': ', 'fz\': ', 'mx\': ', 'my\': ', 'mz\': ']
+                res_labels = ['time: ',
+                                'fx\': ', 'fy\': ', 'fz\': ', 'mx\': ', 'my\': ', 'mz\': ']
 
-                    if (self.print_to_screen):
-                        result_msg = 'Level force evaluation for model part ' + \
-                            self.model_part_name + '\n' \
-                            + ' and level ' + str(idx) + '\n'
-                        result_msg += ', '.join([a+b for a,
-                                                 b in zip(res_labels, output_vals)])
-                        self._PrintToScreen(result_msg, idx)
+                if (self.print_to_screen):
+                    result_msg = 'Level force evaluation for model part ' + \
+                        self.model_part_name + '\n' \
+                        + ' and level ' + str(l_id) + '\n'
+                    result_msg += ', '.join([a+b for a,
+                                                b in zip(res_labels, output_vals)])
+                    self._PrintToScreen(result_msg, l_id)
 
-                    if (self.write_output_file):
-                        self.levels[idx]['output_file'].write(
-                            ' '.join(output_vals) + '\n')
+                    print()
+                if (self.write_output_file):
+                    self.level_forces[l_id]['output_file'].write(
+                        ' '.join(output_vals) + '\n')
 
-                        # NOTE: forcing flush
-                        # check in TimeBasedAsciiFileWriterUtility why this is not handled properly
-                        self.levels[idx]['output_file'].flush()
+                    # NOTE: forcing flush
+                    # check in TimeBasedAsciiFileWriterUtility why this is not handled properly
+                    self.level_forces[l_id]['output_file'].flush()
 
     def ExecuteFinalize(self):
         '''Close output files.'''
         if (self.model_part.GetCommunicator().MyPID() == 0):
-            for idx in range(len(self.levels)):
-                self.levels[idx]['output_file'].close()
+            for l_id in range(len(self.level_forces)):
+                self.level_forces[l_id]['output_file'].close()
 
-    def _EvaluateLevelForces(self, idx):
+    def _EvaluateLevelForces(self, l_id):
         # flow-attached forces: in x-y-z coordinate system
         fb = [0.0, 0.0, 0.0]
         mb = [0.0, 0.0, 0.0]
 
-        for node_id in self.levels[idx]['node_ids']:
+        for node_id in self.level_forces[l_id]['node_ids']:
             node = self.model_part.Nodes[node_id]
 
             # for FluidDynamics: each node has REACTION as a solution step value
@@ -178,9 +280,9 @@ class ApplyLevelForceProcess(KratosMultiphysics.Process):
             fb[1] += nodal_force[1]
             fb[2] += nodal_force[2]
 
-            x = node.X - self.levels[idx]['center'][0]
-            y = node.Y - self.levels[idx]['center'][1]
-            z = node.Z - self.levels[idx]['center'][2]
+            x = node.X - self.level_forces[l_id]['center_coords'][0]
+            y = node.Y - self.level_forces[l_id]['center_coords'][1]
+            z = node.Z - self.level_forces[l_id]['center_coords'][2]
             mb[0] += y * nodal_force[2] - z * nodal_force[1]
             mb[1] += z * nodal_force[0] - x * nodal_force[2]
             mb[2] += x * nodal_force[1] - y * nodal_force[0]
@@ -190,22 +292,22 @@ class ApplyLevelForceProcess(KratosMultiphysics.Process):
 
         return fb, mb
 
-    def _GetFileHeader(self, idx):
-        header = "# Level force for level " + str(idx) + "\n"
+    def _GetFileHeader(self, l_id):
+        header = "# Level force for level " + str(l_id) + "\n"
         header += "# start: " + ', '.join(str(coord)
-                                          for coord in self.levels[idx]['start']) + "\n"
+                                          for coord in self.level_forces[l_id]['start_coords']) + "\n"
         header += "# center: " + ', '.join(str(coord)
-                                           for coord in self.levels[idx]['center']) + "\n"
+                                           for coord in self.level_forces[l_id]['center_coords']) + "\n"
         header += "# end: " + ', '.join(str(coord)
-                                        for coord in self.levels[idx]['end']) + "\n"
+                                        for coord in self.level_forces[l_id]['end_coords']) + "\n"
         header += "# as part of " + \
             self.model_part_name + "\n"
         header += '# Time Fx\' Fy\' Fz\' Mx\' My\' Mz\'\n'
 
         return header
 
-    def _PrintToScreen(self, result_msg, idx):
+    def _PrintToScreen(self, result_msg, l_id):
         KratosMultiphysics.Logger.PrintInfo(
-            'ComputeLevelForceProcess', 'Level ' + str(idx) + ' - flow- and body-attached:')
+            'ComputeLevelForceProcess', 'Level ' + str(l_id) + ' - flow- and body-attached:')
         KratosMultiphysics.Logger.PrintInfo(
             'ComputeLevelForceProcess', 'Current time: ' + result_msg)
---
+++ test3dcsm.gid/fem_analogy_3d_utilities.py
@@ -0,0 +1,187 @@
+
+import numpy as np
+
+#############################
+# OWN function definition START
+
+DIMENSION = 3
+DOFS_PER_NODE = 6
+ERR_ABS_TOL = 1e-8
+
+def setup_fem_beam_analogy(nodal_coordinates, nodes_geom_center, E=10000.0, A=100.0, I=1000.0):
+   
+    def get_unit_vector(v):
+        return v / np.linalg.norm(v)
+
+    def get_direction_cosine(v1, v2):
+        return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
+    def get_length(p1, p2):       
+        return np.linalg.norm(np.subtract(p2, p1))
+    
+    k_total_global =np.zeros((DOFS_PER_NODE + DIMENSION*len(nodal_coordinates), DOFS_PER_NODE + DIMENSION*len(nodal_coordinates)))
+    
+    # coord syst
+    # unit direction vectors
+    x_dir = np.array([1,0,0])
+    y_dir = np.array([0,1,0])
+    z_dir = np.array([0,0,1])
+    # orig_syst = np.array([0,0,0])
+    
+    for idx, node in enumerate(nodal_coordinates):
+        L = get_length(nodes_geom_center, node)
+        
+        k_u = E*A/L 
+        k_vv = 3*E*I/L**3
+        k_vr = 3*E*I/L**2
+        k_rr = 3*E*I/L
+        
+        k_elem_local = np.array([
+            # first node - displacements and rotations
+            [ k_u,   0.0,   0.0,  0.0,   0.0,   0.0, -k_u,   0.0,   0.0],
+            [ 0.0,  k_vv,   0.0,  0.0,   0.0,  k_vr,  0.0, -k_vv,   0.0],
+            [ 0.0,   0.0,  k_vv,  0.0, -k_vr,   0.0,  0.0,   0.0, -k_vv],
+            [ 0.0,   0.0,   0.0,  0.0,   0.0,   0.0,  0.0,   0.0,   0.0],
+            [ 0.0,   0.0, -k_vr,  0.0,  k_rr,   0.0,  0.0,   0.0,  k_vr],
+            [ 0.0,  k_vr,   0.0,  0.0,   0.0,  k_rr,  0.0, -k_vr,   0.0],
+            # second node - only displacements
+            [-k_u,   0.0,   0.0,  0.0,   0.0,   0.0,  k_u,   0.0,   0.0],
+            [ 0.0, -k_vv,   0.0,  0.0,   0.0, -k_vr,  0.0,  k_vv,   0.0],
+            [ 0.0,   0.0, -k_vv,  0.0,  k_vr,   0.0,  0.0,   0.0,  k_vv]])
+       
+        # using the syntax from here https://github.com/airinnova/framat/blob/4177a95b4ed8d95a8330365e32ca13ac9ef24640/src/framat/_element.py
+        # and here https://www.engissol.com/Downloads/Technical%20Notes%20and%20examples.pdf
+        x_elem = get_unit_vector(np.subtract(node, nodes_geom_center))
+        
+        # take global z_dir as global up 
+        if abs(1 - abs(np.dot(x_elem, z_dir))) <= ERR_ABS_TOL:
+            # up-direction and local x-axis are parallel
+            # taking global y_dir as y_elem
+            y_elem = get_unit_vector(np.copy(y_dir))
+        else:
+            y_elem = get_unit_vector(np.cross(z_dir, x_elem))
+        z_elem = get_unit_vector(np.cross(x_elem, y_elem))
+                
+        #######
+        lx = get_direction_cosine(x_elem, x_dir)
+        ly = get_direction_cosine(y_elem, x_dir)
+        lz = get_direction_cosine(z_elem, x_dir)
+        mx = get_direction_cosine(x_elem, y_dir)
+        my = get_direction_cosine(y_elem, y_dir)
+        mz = get_direction_cosine(z_elem, y_dir)
+        nx = get_direction_cosine(x_elem, z_dir)
+        ny = get_direction_cosine(y_elem, z_dir)
+        nz = get_direction_cosine(z_elem, z_dir)
+
+        T3 = np.array([[lx, mx, nx], [ly, my, ny], [lz, mz, nz]])
+        t_elem = np.zeros((DIMENSION**2, DIMENSION**2))
+        t_elem[0:DIMENSION, 0:DIMENSION] = t_elem[DIMENSION:2*DIMENSION, DIMENSION:2*DIMENSION] = t_elem[2*DIMENSION:3*DIMENSION, 2*DIMENSION:3*DIMENSION] = T3
+        
+        k_elem_global = np.matmul(np.matmul(np.transpose(t_elem), k_elem_local), t_elem)
+        
+        # add diagonally-clustered entries corresponding to the starting node - i.e center node - of the beam
+        # forces and moments
+        for i in range(DOFS_PER_NODE):
+            for j in range(DOFS_PER_NODE):
+                k_total_global[i,j] += k_elem_global[i,j]
+                
+        # add diagonally-clustered entries corresponding to the end node of of the beam
+        for i in range(DIMENSION):
+            for j in range(DIMENSION):
+                k_total_global[DOFS_PER_NODE + idx*DIMENSION + i, DOFS_PER_NODE + idx*DIMENSION + j] += k_elem_global[DOFS_PER_NODE+i,DOFS_PER_NODE+j]
+                
+        # add coupling terms between nodes, which are off-diagonal
+        for i in range(DIMENSION):
+            for j in range(DOFS_PER_NODE):
+                # lower diagonal
+                k_total_global[DOFS_PER_NODE + idx*DIMENSION + i, j] += k_elem_global[DOFS_PER_NODE+i,j]
+                # upper diagonal
+                k_total_global[j, DOFS_PER_NODE + idx*DIMENSION + i] += k_elem_global[j,DOFS_PER_NODE+i]
+    
+    return k_total_global
+
+def map_forces_to_nodes(stiffness_matrix, target_resultants):
+    
+    ########
+    # setup a stiffness matrix assuming the center node being connected to all other nodes
+    # by a beam
+
+    # calculate the 6 deformations - 3 translations and 3 rotation - of the center node
+    # using the displacement method in 3D
+    # solved by reduction, only the center node is unconstrained
+    
+    ########
+    # solve
+    center_node_deformations = np.linalg.solve(stiffness_matrix[:DOFS_PER_NODE,:DOFS_PER_NODE], target_resultants)
+    
+    # setup the deformation vector - apart from the center node deformations
+    # all are zero translations due to the pinned support
+    all_deformations = np.zeros(stiffness_matrix.shape[0])
+    # only nonzero which were previously solved for
+    all_deformations[:DOFS_PER_NODE] = center_node_deformations
+    
+    # recover all forces
+    all_forces = np.dot(stiffness_matrix,all_deformations)
+    # first 6 are the recovered resultants
+    recovered_resultants = all_forces[:DOFS_PER_NODE]
+    # the remaining are the actual unknowns of the problem
+    # return the reaction forces from each pinned node
+    # flip sign for consistency
+    nodal_forces = -all_forces[DOFS_PER_NODE:]
+    
+    # check target forces in the center node
+    residual = np.subtract(target_resultants, recovered_resultants)
+    norm_of_residual = np.linalg.norm(residual)
+    print("Residual check in map_forces_to_nodes: " , str(norm_of_residual))
+    if norm_of_residual > ERR_ABS_TOL:
+        raise Exception("Norm of residual too large, check algorithm!")
+
+    return nodal_forces, center_node_deformations
+
+def check_resultant(nodal_coordinates, nodes_geom_center, nodal_forces, target_resultants):
+
+    ##########################
+    # check based on nodal position
+    n_nodes = len(nodal_coordinates)
+
+    mapping_coef_matrix = np.zeros((DOFS_PER_NODE, DIMENSION*n_nodes))
+
+    # fx contribution
+    mapping_coef_matrix[0, 0::DIMENSION] = 1.0
+    # fy contribution
+    mapping_coef_matrix[1, 1::DIMENSION] = 1.0
+    # fz contribution
+    mapping_coef_matrix[2, 2::DIMENSION] = 1.0
+    # mx, my, mz
+    for i in range(0, n_nodes):
+        # dx, dy, dz
+        dx = nodal_coordinates[i][0] - nodes_geom_center[0]
+        dy = nodal_coordinates[i][1] - nodes_geom_center[1]
+        dz = nodal_coordinates[i][2] - nodes_geom_center[2]
+
+        # mx = dy * fz - dz * fy
+        # fy contribution
+        mapping_coef_matrix[3, DIMENSION*i+1] = -dz
+        # fz contribution
+        mapping_coef_matrix[3, DIMENSION*i+2] = dy
+
+        # my = dz * fx - dx * fz
+        # fx contribution
+        mapping_coef_matrix[4, DIMENSION*i+0] = dz
+        # fz contribution
+        mapping_coef_matrix[4, DIMENSION*i+2] = -dx
+
+        # mz = dx * fy - dy * fx
+        # fx contribution
+        mapping_coef_matrix[5, DIMENSION*i+0] = -dy
+        # fy contribution
+        mapping_coef_matrix[5, DIMENSION*i+1] = dx
+
+    recovered_resultants = np.dot(mapping_coef_matrix, nodal_forces)
+    residual = np.subtract(recovered_resultants, target_resultants)
+    norm_of_residual = np.linalg.norm(residual)
+    print("Residual check in check_resultants: " , str(norm_of_residual))
+    if norm_of_residual > ERR_ABS_TOL:
+        raise Exception("Norm of residual too large, check algorithm!")
+
+    return not(norm_of_residual > ERR_ABS_TOL)
